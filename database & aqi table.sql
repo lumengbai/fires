43a00b0f128926cbebcd7b6e00010b07f990d563
n4bil <nnabil@gmail.com>
2018-12-01 21:17:58 -0800

    cleaned up sql script, deleted old sample file

--- database & aqi table.sql
+++ database & aqi table.sql
@@ -1,10 +1,9 @@
 create database fires
 
 use database fires
-CREATE TABLE `fires`.`new_table` (
-  `index` INT NOT NULL,
-  `date` VARCHAR(45) NULL,
-  `overall_aqi` INT NULL,
+CREATE TABLE `fires`.`aqi_data` (
+  `date` VARCHAR(45) NOT NULL,
+  `overall_aqi` INT(11) NULL,
   `site_name` VARCHAR(45) NULL,
-  `year` INT NULL,
-  PRIMARY KEY (`index`));
+  `year` INT(11) NULL,
+  PRIMARY KEY (`date`));
